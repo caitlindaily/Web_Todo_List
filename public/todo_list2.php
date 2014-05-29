d5312999ac5bd43c2f040992cbe2048161d965d8
sweetrocker22 <cdaily87@gmail.com>
2014-05-29 14:42:57 -0500

    When item is added, it can save

--- public/todo_list2.php
+++ public/todo_list2.php
@@ -1,9 +1,12 @@
 <?php
 
 $todo_items = [];
-function open() 
+// //add the
+// $newTodo = $_POST[''];
+// $todo_items = $newTodo;
+//==================================================
+function open($filename = 'list_items.txt') 
 {
-    $filename = 'list_items.txt';
     $filesize = filesize($filename);
     $content = fopen($filename, 'r');
     $fileString = trim(fread($content, $filesize));
@@ -11,35 +14,62 @@ function open()
     fclose($content);
 	return $file;
 }
-$listItems = open();
-foreach ($listItems as $list_item) {
-	array_push($todo_items, $list_item);
+//===================================================
+function save($list, $filename = 'list_items.txt')
+{   
+    $handle = fopen($filename, 'w');
+    $string = implode("\n", $list);
+    fwrite($handle, $string . PHP_EOL);
+    fclose($handle); 
 }
+//====================================================	
+ $listItems = open();
+
+// foreach ($listItems as $list_item) {
+// 	array_push($todo_items, $list_item);
+// }
+// foreach($_POST as $value) {
+// 	array_push($todo_items, $value);
+// }
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-<?php
+// <?php
 	var_dump($_GET);
 	var_dump($_POST);
 ?>	
 	<title>TODO List</title>
 </head>
 <body>
+
+	<form method="POST">
+		<label for="item"></label>
+		<input id="item" name="item" type="text" placeholder="Enter new item">
+		<button type="submit">Submit</button>
+		
+	</form>
+
 	<h1>TODO List</h1>
+
 		<ul>
 			<?php
-			foreach ($todo_items as $todo_item) {
-				echo "<li>$todo_item</li>\n";
+			
+			if (!empty($_POST)) {
+				$listItems[] = "{$_POST['item']}";
+				save($listItems);
 			}
+
+			foreach ($listItems as $item) {
+				echo "<li>$item</li>";
+			}
+			
+
 			?>
+
 		</ul>
 	<h2>Add an item:</h2>	
-<form method="POST">
-	<label for="add"></label>
-	<input id="add" name="add" type="text" placeholder="Enter new item">
-	<button type="submit">Submit</button>
-</form>		
+		
 </body>
 </html>
